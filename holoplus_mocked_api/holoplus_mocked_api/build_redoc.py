498257838e366c32580334372dccd830942cf772
Jiří Kuneš <jirka642@gmail.com>
2025-11-29 19:18:13 +0100

    [FIX] Correct param name

--- holoplus_mocked_api/holoplus_mocked_api/build_redoc.py
+++ holoplus_mocked_api/holoplus_mocked_api/build_redoc.py
@@ -10,6 +10,6 @@ from .app import create_app
 if __name__ == "__main__":
     logging.getLogger("httpx").setLevel(logging.ERROR)
 
-    with TestClient(app=create_app(render_plugins=[RedocRenderPlugin(version="latest")])) as client:
+    with TestClient(app=create_app(plugins=[RedocRenderPlugin(version="latest")])) as client:
         response = client.get("/schema")
         print(response.text)  # noqa: T201
